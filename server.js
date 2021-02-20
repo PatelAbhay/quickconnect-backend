a2f040518e24ca569286337d06fb6889ee12ca92
Sk Sami <sk.sami.aljabar@gmail.com>
2021-02-19 21:40:30 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,80 @@
+var express = require("express");
+var morgan = require("morgan");
+const bodyParser = require("body-parser");
+var path = require("path");
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+// var otherRoutes = require("./routes/routes");
+
+// Joining directories into one
+// app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "views")));
+
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(bodyParser.json({limit: '50mb', extended: true}));
+
+// Logs all request information and time
+app.use(morgan("tiny"));
+
+
+// Database Connection with MySQL
+// dbHandler.connectToDatabase();
+
+// Routing
+// app.use("/ws/", otherRoutes);
+
+var users = [
+  {
+    name: 'a',
+    age: 12,
+  },
+  {
+    name: 'b',
+    age: 32,
+  },
+  {
+    name: 'c',
+    age: 23,
+  },
+]
+
+app.get("/", function(req, res) {
+  try {
+    res.send("Hello World");
+  } catch(e) {
+    res.send({ result: "Test Result"});
+  }
+});
+
+app.get("/test", function(req, res) {
+  try {
+    res.send({ result: "Test Result"});
+  } catch(e) {
+    res.send({ result: "Test Result"});
+  }
+});
+app.post("/users", function(req, res) {
+  var email = req.body.email;
+  var password = req.body.password;
+  console.log("email: ", email);
+  try {
+    res.send({ result: "Test Result"});
+  } catch(e) {
+      res.send("Error C-001: ", e);
+  }
+});
+app.get("/getUsers", function(req, res) {
+  try {
+    res.send({ result: users});
+  } catch(e) {
+    res.send({ result: "Test Result"});
+  }
+});
+app.listen(PORT, function() {
+  console.log("Server started on port " + PORT + "...");
+});
+// setInterval(intervalFunc, 1500);
+// function intervalFunc() {
+//     console.log('Timer yay!');
+// }
